d2c4de10783382c075afcd58fbd08347c03c9446
Mr-fangao <91820025+Mr-fangao@users.noreply.github.com>
2022-04-10 20:04:33 +0800

    项目初始化
    1

---
+++ src/main.js
@@ -0,0 +1,41 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import "./assets/css/global.css";
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+// import 'font-awesome/css/font-awesome.min.css';
+
+// import 'cesium/Source/Widgets/widgets.css'
+// import Cesium from '../node_modules/cesium/Build/Cesium'
+// import Cesium from 'cesium/Source/Cesium'
+// Vue.prototype.Cesium = Cesium
+
+// import vueSeamlessScroll from "vue-seamless-scroll";
+// Vue.use(vueSeamlessScroll);
+
+import 'jquery'
+
+// 引入echarts
+import * as echarts from 'echarts';
+import 'echarts-gl'
+Vue.prototype.$echarts = echarts
+
+// import mapBoxGl from 'mapbox-gl'
+// Vue.prototype.$mapboxgl = mapBoxGl
+
+Vue.config.productionTip = false
+Vue.use(ElementUI);
+
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: {
+    App
+  },
+  template: '<App/>'
+})
